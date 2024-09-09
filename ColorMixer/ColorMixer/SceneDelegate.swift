7ecf0c63b2727f8a1c498eaf29e497891192ff15
Dmitry <mrsahadov@gmail.com>
2024-09-08 23:05:57 -0500

    Initial commit

---
+++ ColorMixer/ColorMixer/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  ColorMixer
+//
+//  Created by Дмитрий Волков on 08.09.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ ColorMixer/ColorMixer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ColorMixer
+//
+//  Created by Дмитрий Волков on 08.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+    }
+
+
+}
+
+private extension ViewController {
+    func setupLayout(){
+        view.backgroundColor = .red
+    }
+}
